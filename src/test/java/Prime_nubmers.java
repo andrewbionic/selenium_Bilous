b0326418a4be6337648a52c3c29df133846f49fe
Abilous <andbilous@gmail.com>
2014-10-31 21:21:28 +0200

    safdasf

---
+++ src/test/java/Prime_nubmers.java
@@ -0,0 +1,30 @@
+/**
+ * Created by c2614 on 20.10.2014.
+ */
+import junit.framework.Assert;
+import org.testng.annotations.Test;
+import java.util.Scanner;
+@Test
+
+public class Prime_nubmers {
+
+    public static boolean isPrimeTest (int x) {
+         boolean var=false;
+        for
+                (int i = 2; i < x; i++) {
+            if (x % i == 0) {
+              var = false;
+            } else
+               var = true;
+        }
+        return var;
+    }
+    public class Prime_nubmersTest {
+
+        @Test
+        private void isPrimeTest(true)  {
+            Assert.assertEquals(true) ;
+
+        }
+    }
+}
